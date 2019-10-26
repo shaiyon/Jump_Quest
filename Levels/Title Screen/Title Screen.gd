5bc714a1c0d99fe5d2d000ac1b3b2bdbfd0e2a10
Flavio <flaviofreitas.h@gmail.com>
2019-10-26 18:08:39 -0300

    SkinButton, Main menu design improved, Google Font BreeSerif

--- Levels/Title Screen/Title Screen.gd
+++ Levels/Title Screen/Title Screen.gd
@@ -2,16 +2,9 @@ extends Control
 
 func _on_Start_pressed():
 	get_tree().change_scene("res://Levels//Level 1//L1.tscn")
-	
-func _on_Change_skin_pressed():
-	# Cycle through 3 skins 
-	global.skin += 1
-	if global.skin > 3:
-		global.skin = 1
-	# Change text of button to show current skin
-	$CenterContainer/VBoxContainer/Change_skin.set_text("Change skin (skin {0})".format([global.skin]))
-	
+
 func _on_Quit_pressed():
 	get_tree().quit()
 
-
+func _on_SkinButton_skin_changed(index: int):
+	global.skin = index + 1
